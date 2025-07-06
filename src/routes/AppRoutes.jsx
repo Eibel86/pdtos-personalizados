67a0cfd045c95e93bbb04bce3f66fd53e9f50969
Eibel86 <afc72455@gmail.com>
2025-07-06 22:13:20 +0200

    ADD: Routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,54 @@
+
+import { Navigate, Route, Routes } from "react-router"
+
+import { HomePage } from "../auth/pages/HomePage"
+import { LoginPage } from "../auth/pages/LoginPage"
+import { RegisterPage } from "../auth/pages/RegisterPage"
+
+import { ProductPage } from "../templateEditor/pages/ProductPage"
+import { EditorPage } from "../templateEditor/pages/EditorPage"
+import { FavouritePage } from "../templateEditor/pages/FavouritePage"
+
+import { PdtoAdminPage } from "../templateEditor/pages/PdtoAdminPage"
+import { TemplateAdminPage } from "../templateEditor/pages/TemplateAdminPage"
+import { ServiceAdminPage } from "../templateEditor/pages/ServiceAdminPage"
+import { useState } from "react"
+
+export const AppRoutes = () => {
+
+    const [isLoged, setsLoged] = useState(false)
+
+    return (
+        <div className='container pt-3'>
+            <p className='h1'>RUTAS APP</p>
+            <hr />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='login' element={<LoginPage />} />
+                <Route path='register' element={<RegisterPage />} />
+
+                {
+                    isLoged && <Route path='user/product' element={<ProductPage />} />
+                }
+                {
+                    isLoged && <Route path='user/editor' element={<EditorPage />} />
+                }
+                {
+                    isLoged && <Route path='user/favourite' element={<FavouritePage />} />
+                }
+
+                {
+                    isLoged && <Route path='admin/product' element={<PdtoAdminPage />} />
+                }
+                {
+                    isLoged && <Route path='admin/editor' element={<TemplateAdminPage />} />
+                }
+                {
+                    isLoged && <Route path='admin/service' element={<ServiceAdminPage />} />
+                }
+
+                <Route path='/*' element={<Navigate to={'/'} />} />
+            </Routes>
+        </div >
+    )
+}
---
+++ src/ui/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div class="p-3 mb-2 bg-info text-black">
+            <h1>
+                Header
+            </h1>
+        </div>
+    )
+}
